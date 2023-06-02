a2e88f5ff3863dcfafdaeeaf9744db3b6989fd7c
diegoandres22 <diego.a.v3005@gmail.com>
2023-06-02 16:20:02 -0300

    dieguito

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function laCajaDePandora(numero) {
-    const diego = "mamaguevo";
-    const tengoTos = 'desdeElMartes';
-    const trabajo = 'Hoy no trabajo';
-    const trabajo2 = 'mañana si :c'
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
+    const nombre = diego;
+    return nombre;
+}
 
+function diego() {
+
+    return {
+        nombre: "diego",
+        edad: 23,
+        nacionalidad: "laMejor"
     }
-
-    return console.log("no me quiero bañar");
 }
