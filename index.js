387454343efeee876755e6fc02f7bf9ee588652f
Luckem67 <lucas.rodriguez.lr99@gmail.com>
2023-06-02 15:31:28 -0300

    pushhhhhhh

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function laCajaDePandora(numero){
     const diego = "mamaguevo";
     const tengoTos = 'desdeElMartes';
 		const trabajo= 'Hoy no trabajo';
-		const trabajo2 = ' mañana si :c'
+		const trabajo2 = 'mañana si :c'
         for (let index = 0; index < array.length; index++) {
             const element = array[index];
             
