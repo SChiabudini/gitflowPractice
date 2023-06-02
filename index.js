d6f14b5565ab9f773e796e962d42e599c706bc43
diegoandres22 <diego.a.v3005@gmail.com>
2023-06-02 15:40:08 -0300

    no me baño

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-function laCajaDePandora(numero){
+function laCajaDePandora(numero) {
     const diego = "mamaguevo";
     const tengoTos = 'desdeElMartes';
-		const trabajo= 'Hoy no trabajo';
-		const trabajo2 = 'mañana si :c'
-        for (let index = 0; index < array.length; index++) {
-            const element = array[index];
-            
-        }
+    const trabajo = 'Hoy no trabajo';
+    const trabajo2 = 'mañana si :c'
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+
+    }
+
+    return console.log("no me quiero bañar");
 }
