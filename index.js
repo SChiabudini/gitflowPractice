8b59f9ac4774a6d17a97f6ba4cc3661bfdb29d0f
Luckem67 <lucas.rodriguez.lr99@gmail.com>
2023-06-02 16:02:06 -0300

    Funcion Lucas

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-function laCajaDePandora(numero) {
-    const diego = "mamaguevo";
-    const tengoTos = 'desdeElMartes';
-    const trabajo = 'Hoy no trabajo';
-    const trabajo2 = 'mañana si :c'
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
+function laCajaDePandora(nombre) {
+    const nombre = 'Lucas';
+		return nombre
+}
 
-    }
-
-    return console.log("no me quiero bañar");
+function Lucas() {
+	return {
+		nombre: "Lucas",
+		edad: 24,
+		nacionalidad: 'Argentina campeon del mundo (MUCHACHOOOS!)'
+	}
 }
