b83ff5e016189dd5b445b8abae4cf94bb95eb452
Matías Morales <programacionfrida@gmail.com>
2023-06-02 15:24:29 -0300

    Eeee

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ function laCajaDePandora(numero){
     const tengoTos = 'desdeElMartes';
 		const trabajo= 'Hoy no trabajo';
 		const trabajo2 = ' mañana si :c'
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+            
+        }
 }
